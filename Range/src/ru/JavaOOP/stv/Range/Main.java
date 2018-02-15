ae585952f577a1aeca84bbcf96663ca710747d66
Tatiana <sychevatatyanav@gmail.com>
2018-02-15 23:54:02 +0700

    Range2 с операциями над множествами

--- Range/src/ru/JavaOOP/stv/Range/Main.java
+++ Range/src/ru/JavaOOP/stv/Range/Main.java
@@ -5,31 +5,10 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Range range = new Range(5, 10);
-        for (; ; ) {
-            System.out.println("Введите начало диапазона:");
-            Scanner scanner = new Scanner(System.in);
-            double from = scanner.nextDouble();
-            System.out.println("Введите последнее число диапазона:");
-            double to = scanner.nextDouble();
-            System.out.println("Введите число");
-            double number = scanner.nextDouble();
-
-            range.setFrom(from);
-            range.setTo(to);
-
-            if (range.getFrom() > range.getTo()) {
-                System.out.println("Перепутаны границы диапазона");
-            }
-
-            double length = range.getLength();
-            System.out.printf("Длина интервала %f%n", length);
-            String str = range.isInside(number) ? "Число попадает в указанный интервал" : "Число не попадает в указанный интервал";
-            System.out.println(str);
-            if (!range.isInside(number)) {
-                break;
-            }
-        }
+        Range2 range1 = new Range2(3, 4);
+        Range2 range2 = new Range2(1, 2);
+        Range2 range_result = range1.getIntersection(range2);
+        Range2[] range_r2 = range1.getDifference(range2);
+        Range2[] r3 = range2.getUnion(range1);
     }
-
 }
