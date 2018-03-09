5614c50310f722f35c365ecd790baf0f4c288446
Tatiana <sychevatatyanav@gmail.com>
2018-03-09 17:39:05 +0700

    Вектор

--- Vector/src/ru/JavaOOP/stv/Vector/Main/Main.java
+++ Vector/src/ru/JavaOOP/stv/Vector/Main/Main.java
@@ -11,44 +11,45 @@ public class Main {
 
         Vector vec3 = new Vector(vec33);
         Vector vec5 = new Vector(vec34);
-        Vector vec4 = new Vector(10, vec33);
+
         String str = vec3.toString();
 
         boolean test1 = str.equals("{1.0, 3.5, 7.8, 4.1 }");
 
-        Vector sum = vec.add(vec3);
-        Vector diff = vec3.subtract(vec3);
-        Vector iSum = sum.invert();
+        vec.add(vec3);
+        vec3.subtract(vec3);
 
-        boolean test2 = sum.equals(new Vector(new double[]{1.0, 3.5, 7.8, 4.1, 0}));
-        boolean test3 = diff.equals(new Vector(new double[]{0, 0, 0, 0}));
-        boolean test4 = iSum.equals(new Vector(new double[]{-1.0, -3.5, -7.8, -4.1, -0.0}));
+        boolean test2 = vec.equals(new Vector(new double[]{1.0, 3.5, 7.8, 4.1, 0}));
+        boolean test3 = vec3.equals(new Vector(new double[]{0, 0, 0, 0}));
 
         vec3.setElementAt(2, -8);
 
-        boolean test5 = vec3.equals(new Vector(new double[]{1.0, 3.5, -8, 4.1}));
+        boolean test5 = vec3.equals(new Vector(new double[]{0, 0, -8, 0}));
 
         vec5.setElementAt(3, vec3.getElementAt(1));
 
-        boolean test6 = vec5.equals(new Vector(new double[]{5, 9.9, 7.4, 3.5, -1, -8.9}));
+        boolean test6 = vec5.equals(new Vector(new double[]{5, 9.9, 7.4, 0.0, -1, -8.9}));
 
-        Vector statVec1 = Vector.addition(sum, diff);
-        Vector statVec2 = Vector.subtraction(iSum, statVec1);
+        Vector statVec1 = Vector.addition(vec, vec3);
+        vec.invert();
+        Vector statVec2 = Vector.subtraction(vec, statVec1);
         double statVecSum = Vector.multiplication(statVec2, statVec1);
 
-        boolean test7 = statVec1.equals(new Vector(new double[]{1.0, 3.5, 7.8, 4.1, 0}));
-        boolean test8 = statVec2.equals(new Vector(new double[]{-2, -7, -15.6, -8.2, -0.0}));
-        boolean test9 = statVecSum == -181.8;
+        boolean test7 = statVec1.equals(new Vector(new double[]{1.0, 3.5, -0.20000000000000018, 4.1, 0}));
+        boolean test8 = statVec2.equals(new Vector(new double[]{-2, -7, -7.6, -8.2, -0.0}));
+        boolean test9 = statVecSum + 58.6 <= 1e-5;
 
         int hash1 = vec.hashCode();
         int hash2 = vec2.hashCode();
         int hash3 = vec3.hashCode();
-        int hash4 = sum.hashCode();
+        Vector testVec = new Vector(new double[]{0, 0, 0, 0, 0});
+        int hash4 = testVec.hashCode();
 
         boolean e1 = vec.equals(vec2);
-        boolean e2 = vec3.equals(sum);
+        boolean e2 = vec3.equals(vec);
+        boolean e3 = vec2.equals(testVec);
 
-        if (test1 && test2 && test3 && test4 && test5 && test6
+        if (test1 && test2 && test3 && test5 && test6
                 && test7 && test8 && test9) {
             System.out.println("Все тесты прошли успешно!");
         } else {
--- Vector/src/ru/JavaOOP/stv/Vector/Vector.java
+++ Vector/src/ru/JavaOOP/stv/Vector/Vector.java
@@ -1,38 +1,39 @@
 package ru.JavaOOP.stv.Vector;
 
 import java.util.Arrays;
-import java.util.Objects;
+
 
 public class Vector {
     private double[] vector;
-    private int size;
+
 
     public double[] getVector() {
         return this.vector;
     }
 
-    public int getSize() {
-        return size;
-    }
-
     public Vector(int n) {
-        if (n <= 0) throw new IllegalArgumentException("неверноый размер вектора");
+        if (n <= 0) {
+            throw new IllegalArgumentException("неверноый размер вектора");
+        }
         vector = new double[n];
-        size = n;
+
     }
 
     public Vector(Vector vector) {
-        this.vector = Arrays.copyOf(vector.vector, vector.size);
-        this.size = vector.getSize();
+        this.vector = Arrays.copyOf(vector.vector, vector.getLength());
     }
 
     public Vector(double[] vector) {
-        size = vector.length;
-        this.vector = Arrays.copyOf(vector, size);
+        if (vector.length == 0) {
+            throw new IllegalArgumentException("неверноый размер вектора");
+        }
+        this.vector = Arrays.copyOf(vector, vector.length);
     }
 
     public Vector(int n, double[] vector) {
-        this.size = n;
+        if (n <= 0) {
+            throw new IllegalArgumentException("неверноый размер вектора");
+        }
         this.vector = Arrays.copyOf(vector, n);
     }
 
@@ -41,8 +42,8 @@ public class Vector {
 
         StringBuilder sb = new StringBuilder();
         sb.append("{");
-        for (int i = 0; i < size; ++i) {
-            sb.append(vector[i]).append(", ");
+        for (double i : vector) {
+            sb.append(i).append(", ");
         }
         sb.delete(sb.length() - 2, sb.length() - 1);
         sb.append("}");
@@ -50,73 +51,67 @@ public class Vector {
         return sb.toString();
     }
 
-    public Vector add(Vector vector) {
-        int n = Math.max(size, vector.size);
-        Vector result = new Vector(n);
-        if (this.size == vector.size) {
+    public void add(Vector vector) {
+        int n = Math.max(this.vector.length, vector.getLength());
+        if (this.getLength() == vector.getLength()) {
             for (int i = 0; i < n; ++i) {
-                result.vector[i] = vector.vector[i] + this.vector[i];
+                this.vector[i] += vector.vector[i];
             }
-        } else if (this.size > vector.size) {
+        } else if (this.getLength() > vector.getLength()) {
             double[] newVector = Arrays.copyOf(vector.vector, n);
             for (int i = 0; i < n; ++i) {
-                result.vector[i] = newVector[i] + this.vector[i];
+                this.vector[i] = newVector[i] + this.vector[i];
             }
         } else {
-            double[] newVector = Arrays.copyOf(this.vector, n);
+            this.vector = Arrays.copyOf(this.vector, n);
             for (int i = 0; i < n; ++i) {
-                result.vector[i] = newVector[i] + this.vector[i];
+                this.vector[i] = this.vector[i] + vector.vector[i];
             }
         }
-        return result;
+
     }
 
-    public Vector subtract(Vector vector) {
-        int n = Math.max(size, vector.size);
-        double[] result = new double[n];
-        if (size == vector.size) {
+    public void subtract(Vector vector) {
+        int n = Math.max(this.getLength(), vector.getLength());
+        if (this.getLength() == vector.getLength()) {
             for (int i = 0; i < n; ++i) {
-                result[i] = this.vector[i] - vector.vector[i];
+                this.vector[i] -= vector.vector[i];
             }
-        } else if (size > vector.size) {
+        } else if (this.getLength() > vector.getLength()) {
             double[] vec2 = Arrays.copyOf(vector.vector, n);
             for (int i = 0; i < n; ++i) {
-                result[i] = this.vector[i] - vec2[i];
+                this.vector[i] -= vec2[i];
             }
         } else {
-            double[] vec2 = Arrays.copyOf(this.vector, n);
+            this.vector = Arrays.copyOf(this.vector, n);
             for (int i = 0; i < n; ++i) {
-                result[i] = vec2[i] - vector.vector[i];
+                this.vector[i] -= vector.vector[i];
             }
         }
-
-        return new Vector(result);
     }
 
-    public Vector multiply(double number) {
-        double[] result = new double[size];
-        for (int i = 0; i < size; ++i) {
-            result[i] = this.vector[i] * number;
+    public void multiply(double number) {
+        for (int i = 0; i < this.getLength(); ++i) {
+            vector[i] *= number;
         }
-        return new Vector(result);
     }
 
-    public Vector invert() {
-        return multiply(-1);
+    public void invert() {
+        multiply(-1);
     }
 
     public void setElementAt(int index, double number) {
-        if (index < 0 || index > size - 1) throw new IllegalArgumentException("неверный индекс");
+        if (index < 0 || index > this.getLength() - 1) throw new IllegalArgumentException("неверный индекс");
         vector[index] = number;
     }
 
     public double getElementAt(int index) {
-        if (index < 0 || index > size - 1) throw new IllegalArgumentException("неверный индекс");
+        if (index < 0 || index > this.getLength() - 1) throw new IllegalArgumentException("неверный индекс");
         return vector[index];
     }
 
     public int getLength() {
-        return size;
+        return this.vector.length;
     }
 
     @Override
@@ -124,82 +119,46 @@ public class Vector {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Vector vector1 = (Vector) o;
-        return size == vector1.size &&
-                Arrays.equals(vector, vector1.vector);
+        return Arrays.equals(getVector(), vector1.getVector());
     }
 
     @Override
     public int hashCode() {
-
-        int result = Objects.hash(size);
-        result = 31 * result + Arrays.hashCode(vector);
-        return result;
+        return Arrays.hashCode(getVector());
     }
 
     public static Vector addition(Vector v1, Vector v2) {
-        int n = Math.max(v1.size, v2.size);
+        int n = Math.max(v1.getLength(), v2.getLength());
         Vector result = new Vector(n);
-
-        if (v1.size == v2.size) {
-            for (int i = 0; i < n; ++i) {
-                result.vector[i] = v1.vector[i] + v2.vector[i];
-            }
-
-        } else if (v1.size > v2.size) {
-            Vector v22 = new Vector(n, v2.vector);
-            for (int i = 0; i < n; ++i) {
-                result.vector[i] = v1.vector[i] + v22.vector[i];
-            }
-
-        } else {
-            Vector v11 = new Vector(n, v1.vector);
-            for (int i = 0; i < n; ++i) {
-                result.vector[i] = v11.vector[i] + v2.vector[i];
-            }
-        }
+        result.vector = Arrays.copyOf(v1.vector, n);
+        result.add(v2);
         return result;
     }
 
     public static Vector subtraction(Vector v1, Vector v2) {
-        int n = Math.max(v1.size, v2.size);
+        int n = Math.max(v1.getLength(), v2.getLength());
         Vector result = new Vector(n);
-
-        if (v1.size == v2.size) {
-            for (int i = 0; i < n; ++i) {
-                result.vector[i] = v1.vector[i] - v2.vector[i];
-            }
-
-        } else if (v1.size > v2.size) {
-            Vector v22 = new Vector(n, v2.vector);
-            for (int i = 0; i < n; ++i) {
-                result.vector[i] = v1.vector[i] - v22.vector[i];
-            }
-
-        } else {
-            Vector v11 = new Vector(n, v1.vector);
-            for (int i = 0; i < n; ++i) {
-                result.vector[i] = v11.vector[i] - v2.vector[i];
-            }
-        }
+        result.vector = Arrays.copyOf(v1.vector, n);
+        result.subtract(v2);
         return result;
     }
 
     public static double multiplication(Vector v1, Vector v2) {
-        int n = Math.max(v1.size, v2.size);
+        int n = Math.max(v1.getLength(), v2.getLength());
         double result = 0;
-        if (v1.size == v2.size) {
+        if (v1.getLength() == v2.getLength()) {
             for (int i = 0; i < n; ++i) {
                 result += v1.vector[i] * v2.vector[i];
             }
-        } else if (v1.size > v2.size) {
-            Vector v22 = new Vector(n, v2.vector);
+        } else if (v1.getLength() > v2.getLength()) {
+            double[] array = Arrays.copyOf(v2.vector, n);
             for (int i = 0; i < n; ++i) {
-                result += v1.vector[i] * v22.vector[i];
+                result += v1.vector[i] * array[i];
             }
         } else {
-            Vector v11 = new Vector(n, v1.vector);
+            double[] array = Arrays.copyOf(v1.vector, n);
             for (int i = 0; i < n; ++i) {
-                result += v11.vector[i] * v2.vector[i];
+                result += array[i] * v2.vector[i];
             }
         }
         return result;
