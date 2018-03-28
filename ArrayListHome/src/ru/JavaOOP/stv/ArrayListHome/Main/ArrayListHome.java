55fc09aaa226743cfff782dfee924c25efa0a014
Tatiana <sychevatatyanav@gmail.com>
2018-03-28 21:28:19 +0700

    ArrayListHome

--- ArrayListHome/src/ru/JavaOOP/stv/ArrayListHome/Main/ArrayListHome.java
+++ ArrayListHome/src/ru/JavaOOP/stv/ArrayListHome/Main/ArrayListHome.java
@@ -8,6 +8,7 @@ import java.util.Scanner;
 
 public class ArrayListHome {
     public static void main(String[] args) throws IOException {
+
         ArrayList<String> strings = new ArrayList<>();
         Scanner scanner = new Scanner(new FileInputStream("strInput.txt"), "windows-1251");
         while (scanner.hasNextLine()) {
@@ -15,15 +16,13 @@ public class ArrayListHome {
         }
 
         System.out.println(strings);
-        ArrayList<Integer> ints = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8));
-        for (int i = 0; i < ints.size(); ++i) {
-            if (ints.get(i) % 2 == 0) {
-                ints.remove(i);
-            }
+        ArrayList<Integer> ints = new ArrayList<>(Arrays.asList(1, 5, 2, 1, 3, 5, 4));
 
-        }
+        ints.removeIf((Integer i) -> i % 2 == 0);
 
-        ArrayList<Integer> ints2 = new ArrayList<>(Arrays.asList(1, 2, 3, 3, 4, 5, 6, 7, 8, 8));
+        System.out.println(ints);
+
+        ArrayList<Integer> ints2 = new ArrayList<>(Arrays.asList(1, 5, 2, 1, 3, 5));
         ArrayList<Integer> ints3 = new ArrayList<>();
 
         for (int i : ints2) {
